82df972a96a25e4ab607bac6b761fb48cb1e1566
Filip Kociok <filipkociok@gmail.com>
2023-07-20 09:05:17 +0200

    Add main page with series listed

--- LingoApp/Models/SeriesModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LingoApp.Models;
-
-public class SeriesModel
-{
-	public int Id { get; set; }
-	public string Name { get; set; }
-	public string Description { get; set; }
-    public List<SeasonModel> Seasons { get; set; }
-}
