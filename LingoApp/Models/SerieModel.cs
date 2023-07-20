97fc793985e8a85b915b338014b3f0ad9f2df395
Filip Kociok <filipkociok@gmail.com>
2023-07-20 09:04:25 +0200

    Correct model name

---
+++ LingoApp/Models/SerieModel.cs
@@ -0,0 +1,11 @@
+﻿namespace LingoApp.Models;
+
+public class SerieModel
+{
+	public int Id { get; set; }
+	public string Name { get; set; }
+	public string Description { get; set; }
+    public List<SeasonModel> Seasons { get; set; }
+    public DateOnly ReleaseDate { get; set; }
+    public string Cover { get; set; }
+}
