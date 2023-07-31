dca2113b46d12b18b976bb2166127fc089cea25e
Filip Kociok <filipkociok@gmail.com>
2023-07-31 18:28:16 +0200

    Add PageStatus controling system

---
+++ LingoLibrary/Enums.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LingoLibrary;
+
+public enum PageStatus { Basic, Loading, NoResultsFound, FoundResults }
+
