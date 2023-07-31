23fd25d64b2b5a71411948ac10f28358da9b26b7
Filip Kociok <filipkociok@gmail.com>
2023-07-31 17:21:21 +0200

    Add method to delete a set from DB

--- Data/LiteDbDataAccess.cs
+++ Data/LiteDbDataAccess.cs
@@ -3,6 +3,7 @@ using LingoLibrary.Models;
 using LiteDB;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 
 namespace Data;
 
@@ -37,7 +38,6 @@ public class LiteDbDataAccess
 
 		var sets = db.GetCollection<SetModel>("sets");
 
-		// Assuming the combination of SerieId, SeasonId, and EpisodeId uniquely identifies a set
 		return sets.FindOne(x => x.EpisodeId == episodeId);
 	}
 
@@ -47,5 +47,23 @@ public class LiteDbDataAccess
 
 		var sets = db.GetCollection<SetModel>("sets");
 		return sets.Update(set);
+	}
+
+	public bool DeleteSet(int episodeId) 
+	{
+		using var db = new LiteDatabase(_databasePath);
+
+		var sets = db.GetCollection<SetModel>("sets");
+
+		var set = sets.FindOne(x => x.EpisodeId == episodeId);
+
+		if (set is null)
+		{
+			Debug.WriteLine($"Could not find set for episode id {episodeId}");
+			return false;
+		}
+
+		sets.Delete(set.Id);
+		return true;
 	}
 }
